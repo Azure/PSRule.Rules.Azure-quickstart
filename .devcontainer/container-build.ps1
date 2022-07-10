2f255e629e9af75198924381fd3f3768b2b71a97
Bernie White <bewhite@microsoft.com>
2022-07-10 07:34:01 +0000

    Install Bicep manually

--- .devcontainer/container-build.ps1
+++ .devcontainer/container-build.ps1
@@ -9,12 +9,9 @@ $ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyC
 if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction Ignore)) {
     Install-PackageProvider -Name NuGet -Force -Scope CurrentUser;
 }
-if ($Null -eq (Get-InstalledModule -Name PowerShellGet -MinimumVersion 2.2.1 -ErrorAction Ignore)) {
-    Install-Module -Name PowerShellGet -MinimumVersion 2.2.1 -Scope CurrentUser -Force -AllowClobber;
+if ($Null -eq (Get-InstalledModule -Name PowerShellGet -MinimumVersion 2.2.5 -ErrorAction Ignore)) {
+    Install-Module -Name PowerShellGet -MinimumVersion 2.2.5 -Scope CurrentUser -Force -AllowClobber;
 }
 if ($Null -eq (Get-InstalledModule -Name PSRule.Rules.Azure -MinimumVersion 1.17.0 -ErrorAction Ignore)) {
     Install-Module -Name PSRule.Rules.Azure -MinimumVersion 1.17.0 -Scope CurrentUser -Force;
 }
-
-# Install Bicep
-az bicep install
