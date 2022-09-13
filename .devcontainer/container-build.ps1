1f1f775a0e5a39d6393542cfd7850762dceab314
Bernie White <bewhite@microsoft.com>
2022-09-13 17:07:32 +1000

    Updates to codespaces config

--- .devcontainer/container-build.ps1
+++ .devcontainer/container-build.ps1
@@ -12,6 +12,6 @@ if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction Ignore)) {
 if ($Null -eq (Get-InstalledModule -Name PowerShellGet -MinimumVersion 2.2.5 -ErrorAction Ignore)) {
     Install-Module -Name PowerShellGet -MinimumVersion 2.2.5 -Scope CurrentUser -Force -AllowClobber;
 }
-if ($Null -eq (Get-InstalledModule -Name PSRule.Rules.Azure -MinimumVersion 1.17.0 -ErrorAction Ignore)) {
-    Install-Module -Name PSRule.Rules.Azure -MinimumVersion 1.17.0 -Scope CurrentUser -Force;
+if ($Null -eq (Get-InstalledModule -Name PSRule.Rules.Azure -MinimumVersion 1.19.2 -ErrorAction Ignore)) {
+    Install-Module -Name PSRule.Rules.Azure -MinimumVersion 1.19.2 -Scope CurrentUser -Force;
 }
