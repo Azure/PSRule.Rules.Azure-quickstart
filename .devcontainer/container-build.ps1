890961286501e0c9e1f00a6ee2e897161d00bbc9
Bernie White <bewhite@microsoft.com>
2025-02-21 15:00:15 +1000

    Update dev container (#50)

--- .devcontainer/container-build.ps1
+++ .devcontainer/container-build.ps1
@@ -4,14 +4,17 @@
 # Note:
 # This is run during container creation.
 
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
 # Install PowerShell dependencies
 $ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue;
 if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction Ignore)) {
     Install-PackageProvider -Name NuGet -Force -Scope CurrentUser;
 }
-if ($Null -eq (Get-InstalledModule -Name PowerShellGet -MinimumVersion 2.2.5 -ErrorAction Ignore)) {
-    Install-Module -Name PowerShellGet -MinimumVersion 2.2.5 -Scope CurrentUser -Force -AllowClobber;
+if ($Null -eq (Get-InstalledModule -Name PowerShellGet -MinimumVersion 2.2.1 -ErrorAction Ignore)) {
+    Install-Module PowerShellGet -MinimumVersion 2.2.1 -Scope CurrentUser -Force -AllowClobber;
 }
-if ($Null -eq (Get-InstalledModule -Name PSRule.Rules.Azure -MinimumVersion 1.19.2 -ErrorAction Ignore)) {
-    Install-Module -Name PSRule.Rules.Azure -MinimumVersion 1.19.2 -Scope CurrentUser -Force;
+if ($Null -eq (Get-InstalledModule -Name InvokeBuild -MinimumVersion 5.4.0 -ErrorAction Ignore)) {
+    Install-Module InvokeBuild -MinimumVersion 5.4.0 -Scope CurrentUser -Force;
 }
--- .devcontainer/container-start.ps1
+++ .devcontainer/container-start.ps1
@@ -8,8 +8,11 @@
 Update-Module PSRule.Rules.Azure -Scope CurrentUser -Force;
 
 # Fetch the latest Bicep CLI binary
-curl -Lo bicep-cli https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64
+curl -Lo bicep https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64
 # Mark it as executable
-chmod +x ./bicep-cli
+chmod +x ./bicep
 # Add bicep to your PATH (requires admin)
-sudo mv ./bicep-cli /usr/local/bin/bicep --force
+sudo mv ./bicep /usr/local/bin/bicep
+# Verify you can now access the 'bicep' command
+bicep --version
+# Done!
